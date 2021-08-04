6e07f180470979fbb97c68621553767af29748c9
Stjepan Ruklić <sr51641@fer.hr>
2021-08-04 14:00:59 +0200

    fix annotation configuration

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -4,12 +4,18 @@ import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"guru.springframework.sfgdi", "com.springframework.pets"}) // used to scan for oder packages for beans
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		PetController petController = ctx.getBean("petController", PetController.class);
+		System.out.println("--- The Best Pet is ---");
+		System.out.println(petController.whichPetIsTheBest());
 
 		System.out.println("------ Profile"); // Currently using en profile - see application.properties
 		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
